2c6d04e18dcfc733fc2072e898b61a5e09b50b91
frju365 <abld@abld.info>
2019-02-22 19:42:02 +0100

    add function work env

--- __ini__.py
+++ __ini__.py
@@ -2,17 +2,9 @@
 
 import urllib
 import os
+from from inc.basic_functions import *
+from from inc.main import *
+from from inc.rbl_checki import *
 
-name_of_the_file=""
 
-def write_to_file(response, name_of_the_file="temp.txt")
-    file = open(name_of_the_file, "a")
-    file.write(response)
-    file.close()
-
-def hasipback(url, secure=true)
-    hostname = url
-    response = os.system("host " + hostname)
-    write_to_file(response)
-    if ()
-    response = os.system("ping -c 1 " + hostname)
+main()
---
+++ inc/main.py
@@ -0,0 +1,5 @@
+from rbl_checki import *
+from basic_functions import *
+
+def main():
+    list = rbl_list()
