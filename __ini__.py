40681bc6943159d42e7c08e135cb5ee73f76930c
frju365 <abld@abld.info>
2019-02-24 02:21:29 +0100

    add

--- __ini__.py
+++ __ini__.py
@@ -2,9 +2,10 @@
 
 import urllib
 import os
-from from inc.basic_functions import *
-from from inc.main import *
-from from inc.rbl_checki import *
+
+from inc.basicfunctions import *
+from inc.main import *
+from inc.rbl_checki import *
 
 
 main()
---
+++ inc/rbl_checki.py
@@ -0,0 +1,1 @@
+def rbl_list():
